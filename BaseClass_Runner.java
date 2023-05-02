b3632ad9e014cd142c731b769850ecea83c4542e
suryasuryasiva <suryasuryasiva001@gmail.com>
2023-05-02 22:22:08 +0530

    important

---
+++ BaseClass_Runner.java
@@ -0,0 +1,12 @@
+package com.java;
+
+
+
+public class BaseClass_Runner extends Base_Class {
+	public static void main(String[] args) {
+		getDriver();
+		getUrl("https://www.google.com/");
+		
+	}
+
+}
